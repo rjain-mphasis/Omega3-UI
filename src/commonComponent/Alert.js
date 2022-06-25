47b65ed694363f6e111eb259129f7b8fef738dcb
MDSHAHBAZ ALAM <mdshahbaz.alam@mphasis.com>
2022-06-25 12:59:11 +0530

    first commit

---
+++ src/commonComponent/Alert.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Alert extends Component {
+  render() {
+    return (
+     
+        <div className='alert'>
+          <h3>This is an example of alert!</h3>
+       </div>
+       
+    )
+  }
+}
---
+++ src/commonComponent/Mock.js
@@ -0,0 +1,20 @@
+
+export const Mock=
+[
+    {footer:[
+        {
+        className:'footer',
+        text:'2022 Copyrights have been reserved for this page'
+        }
+           ]
+    },
+    {
+        header:[
+            {
+        src:'https://www.bookmyhiring.com/wp-content/uploads/2020/03/Mphasis-logo-scaled.jpg',
+        alt:'mphasisLogo',
+        name:'MD SHAHBAZ ALAM'
+            }
+        ]
+    }
+]
